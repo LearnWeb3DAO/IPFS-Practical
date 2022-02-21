8c979882d5bc49260eb1efb67941fa345d1a22a2
Sneh Koul <snehkoul1999@gmail.com>
2022-02-21 15:26:25 -0500

    chnage env names

--- hardhat-tutorial/hardhat.config.js
+++ hardhat-tutorial/hardhat.config.js
@@ -3,14 +3,14 @@ require("dotenv").config({ path: ".env" });
 
 const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
 
-const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
+const MUMBAI_PRIVATE_KEY = process.env.MUMBAI_PRIVATE_KEY;
 
 module.exports = {
   solidity: "0.8.4",
   networks: {
     mumbai: {
       url: ALCHEMY_API_KEY_URL,
-      accounts: [RINKEBY_PRIVATE_KEY],
+      accounts: [MUMBAI_PRIVATE_KEY],
     },
   },
 };
